646264c2bcb33d7576ac6bb6913ac17228df76c6
Sanjay Kumar <onlinewebtutorhub@gmail.com>
2022-07-01 09:10:13 +0530

    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

---
+++ php-mysqli-form-validation/process.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ @Author: Sanjay Kumar
+ @Project: PHP MySQLi Form Validation Using jQuery Tutorial
+ @Email: onlinewebtutorhub@gmail.com
+ @Website: https://onlinewebtutorblog.com/
+*/
+
+// Include Database
+include './dbconfig.php';
+
+$name = htmlspecialchars(trim($_POST['username']));
+$email = htmlspecialchars(trim($_POST['email']));
+$pass = htmlspecialchars(trim($_POST['password']));
+$mobile = htmlspecialchars(trim($_POST['mobile']));
+
+if (empty($name) || empty($email) || empty($pass) || empty($mobile)) {
+
+    echo '<div class="alert alert-success">Please fill all required field</div>';
+
+    exit();
+} else {
+
+    $sql = "INSERT INTO users(username,email,password,mobile) VALUES (?, ?, ?, ?)";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ssss", $name, $email, $pass, $mobile);
+
+    $stmt->execute();
+
+    echo '<div class="alert alert-success">Data Successfully Inserted</div>';
+
+    exit();
+}
