ef4bebc10c2def6ccf2f9288875ffa92cc3089a4
Sanjay Kumar <onlinewebtutorhub@gmail.com>
2022-07-05 09:07:36 +0530

    <?php

---
+++ php-xml-to-array/index.php
@@ -0,0 +1,16 @@
+<?php
+/*
+ @Author: Sanjay Kumar
+ @Project: How To Read XML Data To Array in PHP Tutorial
+ @Email: onlinewebtutorhub@gmail.com
+ @Website: https://onlinewebtutorblog.com/
+*/
+
+$xmlString = file_get_contents('books.xml');
+$xmlObject = simplexml_load_string($xmlString);
+
+$json = json_encode($xmlObject);
+$phpArray = json_decode($json, true);
+
+echo "<pre>";
+print_r($phpArray);
